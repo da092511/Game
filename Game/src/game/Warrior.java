3bff2c47bb3ed7690c3fac49ad1397dbad726180
dayeon <da092511@naver.com>
2024-04-01 18:03:19 +0900

    Add : skill method

---
+++ Game/src/game/Warrior.java
@@ -0,0 +1,14 @@
+package game;
+
+public class Warrior extends Player {
+
+	public Warrior(String na, int max, int pow) {
+		super(na, max, pow);
+	}
+	
+	@Override
+	public void skill() {
+		super.skill();
+	}
+
+}
