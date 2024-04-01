3c631bb68a34702a03e39b9d6e252250801a2c1e
dayeon <da092511@naver.com>
2024-04-01 17:10:52 +0900

    Add : method(getCurHp, setCurHp...)

--- Game/src/game/Unit.java
+++ Game/src/game/Unit.java
@@ -1,11 +1,11 @@
 package game;
 
-public abstract class Unit {
-	int curHp;
-	int maxHp;
-	int power;
-	String name;
-	String state = "노말";
+public class Unit {
+	private int curHp;
+	private int maxHp;
+	private int power;
+	private String name;
+	private String state = "노말";
 	
 	public Unit() {
 		
@@ -31,8 +31,29 @@ public abstract class Unit {
 		power = pw;
 	}
 	
+	public int getCurHp() {
+		return this.curHp;
+	}
+	
+	public void setCurHp(int curHp) {
+		this.curHp = curHp;
+	}
+	
 	public void attack(Unit target) {
+		int hp = target.getCurHp() - power;
 		
+		System.out.println("[" + name + "] 이 " + "[" + target.name + "] 에게 " + power + "의 데미지를 입힙니다. ");
+		if (hp <= 0) {
+			System.out.println("[" + target.name + "] 을 쳐치했습니다.");
+			target.setCurHp(0);
+			return;
+		}
+		
+		target.setCurHp(hp);
+	}
+	
+	public void printData() {
+		System.out.println("[" + name + "] [" + curHp + "/" + maxHp + "] [" + power + "]");
 	}
 	
 	
