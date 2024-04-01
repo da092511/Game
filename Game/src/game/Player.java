ce09c1fdc6126746ccca7047c5840a742f71db0c
dayeon <da092511@naver.com>
2024-04-01 17:14:39 +0900

    Add : skill method

--- Game/src/game/Player.java
+++ Game/src/game/Player.java
@@ -1,5 +1,12 @@
 package game;
 
-public class Player {
-
+public class Player extends Unit {
+	
+	public Player(String na, int max, int pow) {
+		super(na,max,pow);
+	}
+	
+	public void skill() {
+		
+	}
 }
