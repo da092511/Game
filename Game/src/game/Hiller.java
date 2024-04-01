215cda7b92574554b5ba23c1b999edae13852ff9
dayeon <da092511@naver.com>
2024-04-01 18:41:50 +0900

    1Day

---
+++ Game/src/game/Hiller.java
@@ -0,0 +1,13 @@
+package game;
+
+public class Hiller extends Unit{
+	
+	public Hiller(String na, int max, int pow) {
+		super(na, max, pow);
+	}
+	
+	public Hiller() {
+		super.setName("힐러");
+	}
+	
+}
---
+++ Game/src/game/Witch.java
@@ -0,0 +1,12 @@
+package game;
+
+public class Witch extends Unit {
+	
+	public Witch(String na, int max, int pow) {
+		super(na,max,pow);
+	}
+	
+	public void skill() {
+		
+	}
+}
