55ea1e0890ba1bfa89cee37b52b57207c0047de0
dayeon <da092511@naver.com>
2024-04-02 14:38:04 +0900

    Add : skill method

--- Game/src/game/Warrior.java
+++ Game/src/game/Warrior.java
@@ -10,5 +10,13 @@ public class Warrior extends Witch {
 	public void skill() {
 		System.out.println("적 전체에게 데미지");
 	}
+	
+	public void skill(Unit target) {
+		int power = super.getPower();
+		System.out.println("[" + super.getName() + "]가 " + "[" + target.getName() + "]에게 " + power + "의 데미지를 입힙니다. ");
+		
+		int hp = target.getCurHp() - power;
+		target.setCurHp(hp);
+	}
 
 }
