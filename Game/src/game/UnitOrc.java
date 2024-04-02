397586acf8d26df827d2bd2195377f1eb8f7ef0d
dayeon <da092511@naver.com>
2024-04-02 14:57:26 +0900

    Add : skill method

--- Game/src/game/UnitOrc.java
+++ Game/src/game/UnitOrc.java
@@ -8,4 +8,16 @@ public class UnitOrc extends Unit{
 	public void skill() {
 		System.out.println("한명에게 2배의 데미지 + 기절 ");
 	}
+	
+	public void skill(Unit target) {
+		int power = super.getPower() * 2;
+		System.out.println("[" + super.getName() + "]가 " + "[" + target.getName() + "]에게 " + power + "의 데미지를 입힙니다. ");
+	
+		if(!target.getIsFaint())
+			target.setFaint();
+		
+		int hp = target.getCurHp() - power;
+		target.setCurHp(hp);
+	
+	}
 }
