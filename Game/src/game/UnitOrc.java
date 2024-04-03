9711af61fbbc9ef80421a16c8f3cc95e77de9324
dayeon <da092511@naver.com>
2024-04-03 10:19:13 +0900

    Modify : skill method

--- Game/src/game/UnitOrc.java
+++ Game/src/game/UnitOrc.java
@@ -18,6 +18,11 @@ public class UnitOrc extends Unit{
 		if(!target.getIsFaint())
 			target.setFaint();
 		
+		if(target.getCurHp() == 0) {
+			System.err.println("스킬 실패 : 이미 죽은 적입니다");
+			return;
+		}
+		
 		int hp = target.getCurHp() - power;
 		if(hp <= 0) {
 			System.out.println("[" + target.getName() + "]을 처치하였습니다.");
