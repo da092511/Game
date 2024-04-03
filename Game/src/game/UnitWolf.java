31bdf9dd8b0203287e0667bdd04841d22bda0237
dayeon <da092511@naver.com>
2024-04-03 10:19:24 +0900

    Modify : skill method

--- Game/src/game/UnitWolf.java
+++ Game/src/game/UnitWolf.java
@@ -13,10 +13,19 @@ public class UnitWolf extends Unit{
 	
 	@Override
 	public void skill(Unit target) {
+		if(target.getCurHp() == 0)
+			return;
+		
 		int power = super.getPower() / 2;
 		System.out.println("[" + super.getName() + "]가 " + "[" + target.getName() + "]에게 " + power + "의 데미지를 입힙니다. ");
 		
-		int hp = target.getCurHp() - power;
+		int hp = target.getCurHp() ;
+		
+		if(hp == 0) {
+			System.err.println("스킬 실패 : 이미 죽은 적입니다");
+		}
+		
+		hp = hp - power;
 		
 		if(hp <= 0) {
 			System.out.println("[" + target.getName() + "]을 처치하였습니다.");
